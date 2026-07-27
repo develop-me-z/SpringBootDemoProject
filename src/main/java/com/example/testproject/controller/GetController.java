e51bd4b5dadf683f3f09df6b217a14a92b522e08
Jisoo Oh <ohjisoo826@gmail.com>
2026-07-27 22:38:09 +0900

    Get 예제

---
+++ src/main/java/com/example/testproject/controller/GetController.java
@@ -0,0 +1,59 @@
+package com.example.testproject.controller;
+
+import com.example.testproject.dto.MemberDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+
+    // http://localhost:8080/api/v1/get-api/hello
+    @RequestMapping(value="/hello", method= RequestMethod.GET)
+    public String getHello(){
+        return "Hello world";
+    }
+
+    @GetMapping(value="/name")
+    public String getName(){
+        return "Flature";
+    }
+
+    @GetMapping(value="/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable){
+        return variable;
+    }
+
+    @GetMapping(value = "/variable2/{variable}")
+    public String getVariable2(@PathVariable("variable") String var){
+        return var;
+    }
+
+    @GetMapping(value="/request1")
+    public String getRequestParam1(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String organization){
+        return name + " " + email + " " + organization;
+    }
+
+    // ?key=value&key2=value2
+    @GetMapping(value="/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param){
+        StringBuilder sb = new StringBuilder();
+
+        param.entrySet().forEach(map->{
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    // ?name=flature&email=dd&organization=ss
+    @GetMapping(value="/request3")
+    public String getRequestParam3(MemberDTO memberDTO){
+        return memberDTO.toString();
+    }
+
+}
