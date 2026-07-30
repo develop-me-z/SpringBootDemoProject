afc4653148fb6a26d359f21f72b63a3a64110a21
Jisoo Oh <ohjisoo826@gmail.com>
2026-07-30 20:08:25 +0900

    logback 추가

--- src/main/java/com/example/testproject/controller/ProductController.java
+++ src/main/java/com/example/testproject/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.example.testproject.controller;
 
 import com.example.testproject.data.dto.ProductDto;
 import com.example.testproject.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,18 +11,29 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/product-api")
 public class ProductController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
     private ProductService productService;
 
     @Autowired
-    public ProductController(ProductService productService){this.productService = productService;}
-
-    @GetMapping(value="/product/{productId}")
-    public ProductDto getProduct(@PathVariable String productId){
-        return productService.getProduct(productId);
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
 
-    @PostMapping(value="/product")
-    public ProductDto createProduct(@RequestBody ProductDto productDto){
+    @GetMapping(value = "/product/{productId}")
+    public ProductDto getProduct(@PathVariable String productId) {
+
+        long startTime = System.currentTimeMillis();
+        LOGGER.info("[ProductController] perform {} of Around Hub API.", "getProduct");
+
+        ProductDto productDto = productService.getProduct(productId);
+        LOGGER.info("[ProductController] Response :: productId = {}, productName = {}, productPrice = {}, productStock = {}, Response Time = {}ms", productDto.getProductId(),
+                productDto.getProductName(), productDto.getProductPrice(), productDto.getProductStock(), (System.currentTimeMillis() - startTime));
+
+        return productDto;
+    }
+
+    @PostMapping(value = "/product")
+    public ProductDto createProduct(@RequestBody ProductDto productDto) {
         String productId = productDto.getProductId();
         String productName = productDto.getProductName();
         int productPrice = productDto.getProductPrice();
@@ -29,7 +42,9 @@ public class ProductController {
         return productService.saveProduct(productId, productName, productPrice, productStock);
     }
 
-    @DeleteMapping(value="/product/{productId}")
-    public ProductDto deleteProduct(@PathVariable String productId){return null;}
+    @DeleteMapping(value = "/product/{productId}")
+    public ProductDto deleteProduct(@PathVariable String productId) {
+        return null;
+    }
 
 }
